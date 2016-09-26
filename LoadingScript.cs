f9b504af933a0ed4ca958be8051568b1801eafc2
123456Li <1523599085@qq.com>
2016-09-26 15:06:19 +0800

    hh
    hh

---
+++ LoadingScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//注释
+//~~~~
+public class LoadingScript : MonoBehaviour {
+	//异步加载
+	private AsyncOperation asy;
+	public UIProgressBar progressB;
+	// Use this for initialization
+	void Start () {
+		if (progressB) {
+			progressB.value = 0;
+		}
+		StartCoroutine ("loadSence");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (asy.progress);
+		//进度条加载到90%，跳转场景
+		if (asy.progress < 0.9) {
+			progressB.value = asy.progress;
+		} else {
+			progressB.value = 1;
+		}
+	}
+	IEnumerator loadSence(){
+		asy = Application.LoadLevelAsync ("sence1");
+		Debug.Log (asy.progress);
+		yield return 0;
+	}
+}
